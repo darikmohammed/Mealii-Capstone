05e53f12d4ea64466153883c8ee849af31b347b6
darikmohammed <r.m.darik@gmail.com>
2022-06-22 02:18:06 +0300

    Add fetching data from API module
    - Create API module for geting the data.
    - Create an event listener for the catagories to display.
    - Create an event listener for the catagories to display the meal for the specific catagory.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,42 @@ import MealAPI from './modules/mealAPI';
 
 const Meals = document.querySelector('#cards');
 
-const mealAPI = new MealAPI(); 
+const mealAPI = new MealAPI();
 
-const displayCatagories = async()=>{
+const displayCatagories = async () => {
   const catagories = await mealAPI.receiveData();
-  console.log(catagories);
-}
+  const catagoriesList = document.querySelector(
+    '.meal-catagories-list .meal-catagories'
+  );
+  catagoriesList.innerHTML = '';
+  catagories.meals.forEach((catagory) => {
+    catagoriesList.innerHTML += `<li>
+    <a href="#" class="catagory-button" id="${catagory.strCategory}">${catagory.strCategory}</a>
+</li>`;
+  });
+  //create an event listener for the meal buttons
+
+  const mealButtons = document.querySelectorAll('.catagory-button');
+  mealButtons.forEach((mealButton) => {
+    mealButton.addEventListener('click', async () => {
+      const allMeal = await mealAPI.generateMeals(
+        mealButton.getAttribute('id')
+      );
+      Meals.innerHTML = '';
+      allMeal.meals.forEach((meal) => {
+        Meals.innerHTML += `<div class="card">
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        <h3>${meal.strMeal}</h3>
+        <button class="comment-btn" id="${meal.idMeal}">Comment</button>
+    </div>`;
+      });
+      // create an event listener for the comment buttons
+
+      const commentButtons = document.querySelectorAll('.comment-btn');
+      commentButtons.forEach((button) => {
+        console.log(button.getAttribute('id'));
+      });
+    });
+  });
+};
 displayCatagories();
--- src/modules/mealAPI.js
+++ src/modules/mealAPI.js
@@ -1,23 +1,32 @@
 class MealAPI {
   constructor() {
-    this.baseURL = 'www.themealdb.com/api/json/v1/1/';
+    this.baseURL = 'https://www.themealdb.com/api/json/v1/1/';
   }
 
   receiveData = async () => {
+    const response = await fetch(`${this.baseURL}list.php?c=list`, {
+      method: 'GET',
+      header: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const allMealsCatagory = await response.json();
+    return allMealsCatagory;
+  };
+
+  generateMeals = async (mealCatagory) => {
     const response = await fetch(
-      'www.themealdb.com/api/json/v1/1/list.php?c=list',
+      `${this.baseURL}filter.php?c=${mealCatagory}`,
       {
         method: 'GET',
         header: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      },
+      }
     );
     const allMeals = await response.json();
     return allMeals;
   };
-
-
 }
 
 module.exports = MealAPI;
