8cf89b92539f40b8ce3b1d0ec8b6d8ad80f391a1
darikmohammed <r.m.darik@gmail.com>
2022-06-24 10:48:33 +0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const commentEventButton = () => {
       modal.style.display = 'block';
       const meals = await mealAPI.getMealDetail(button.getAttribute('id'));
       const comments = await involvementAPI.getComment(
-        button.getAttribute('id')
+        button.getAttribute('id'),
       );
       // add comment counter
       let commentCount = 0;
@@ -28,7 +28,7 @@ const commentEventButton = () => {
         commentCount = comments.length;
       }
       document.querySelector(
-        '.meal-comment-list .message-count'
+        '.meal-comment-list .message-count',
       ).textContent = `${commentCount} Comments`;
       // display comment
       const commentDiv = document.querySelector('.meal-comments');
@@ -56,18 +56,16 @@ const commentEventButton = () => {
         .querySelector('#comment-form button')
         .setAttribute('id', meals.meals[0].idMeal);
       document.querySelector(
-        '.meal-thumb'
+        '.meal-thumb',
       ).innerHTML = `<img src="${meals.meals[0].strMealThumb}"
       alt="${meals.meals[0].strMeal}">`;
-      document.querySelector('.meal-title').textContent =
-        meals.meals[0].strMeal;
+      document.querySelector('.meal-title').textContent = meals.meals[0].strMeal;
       document.querySelector(
-        '.meal-category'
+        '.meal-category',
       ).textContent = `${meals.meals[0].strCategory}, ${meals.meals[0].strArea}`;
-      document.querySelector('.meal-instructions').textContent =
-        meals.meals[0].strInstructions;
+      document.querySelector('.meal-instructions').textContent = meals.meals[0].strInstructions;
       document.querySelector(
-        '.meal-youtube'
+        '.meal-youtube',
       ).innerHTML = `<a href="${meals.meals[0].strYoutube}"><i
         class="fa-brands fa-youtube"></i>
     <p>YouTube</p>
@@ -83,7 +81,7 @@ const likeEventButton = () => {
     button.addEventListener('click', async () => {
       await involvementAPI.postLike(button.getAttribute('id'));
       const statusUpdate = document.querySelector(
-        `.new-like-status-${button.getAttribute('id')}`
+        `.new-like-status-${button.getAttribute('id')}`,
       );
       statusUpdate.innerHTML = 'Liked!';
       statusUpdate.style.display = 'block';
@@ -94,10 +92,10 @@ const likeEventButton = () => {
       // update the like display
       const likes = await involvementAPI.getLikes();
       const item = likes.find(
-        (element) => element.item_id === button.getAttribute('id')
+        (element) => element.item_id === button.getAttribute('id'),
       );
       const likeDisplaySpan = document.querySelector(
-        `#span-${button.getAttribute('id')}`
+        `#span-${button.getAttribute('id')}`,
       );
       likeDisplaySpan.innerHTML = `${item.likes} Likes`;
     });
@@ -107,7 +105,7 @@ const likeEventButton = () => {
 const displayCatagories = async () => {
   const catagories = await mealAPI.receiveData();
   const catagoriesList = document.querySelector(
-    '.meal-catagories-list .meal-catagories'
+    '.meal-catagories-list .meal-catagories',
   );
   catagoriesList.innerHTML = '';
   catagories.meals.forEach((catagory) => {
@@ -148,12 +146,12 @@ const displayCatagories = async () => {
   mealButtons.forEach((mealButton) => {
     mealButton.addEventListener('click', async () => {
       const allMeal = await mealAPI.generateMeals(
-        mealButton.getAttribute('id')
+        mealButton.getAttribute('id'),
       );
       const likes = await involvementAPI.getLikes();
       Meals.innerHTML = '';
       mealCategoryHeader.textContent = `Our ${mealButton.getAttribute(
-        'id'
+        'id',
       )} Meal Category`;
       allMeal.meals.forEach((meal) => {
         let item = likes.find((element) => element.item_id === meal.idMeal);
@@ -213,7 +211,7 @@ commentForm.addEventListener('submit', async (e) => {
       commentCount = comments.length;
     }
     document.querySelector(
-      '.meal-comment-list .message-count'
+      '.meal-comment-list .message-count',
     ).textContent = `${commentCount} Comments`;
     // add formbutton id
     const commentDiv = document.querySelector('.meal-comments');
